7e84b2f0e7f0a9014946383b86baaec177c0ab68
Rebecca Nesson <rebecca@prx.org>
2012-11-14 21:51:04 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lurc::Application.config.secret_token = '0e6b1987979a3802b76afdd79b6dc81d2646fdd8ed62e4106a8970b78364bbe8957b694d7cb7c872a025c8d3c22b12c36a2df69002d1d6d026e51e42e6829824'
