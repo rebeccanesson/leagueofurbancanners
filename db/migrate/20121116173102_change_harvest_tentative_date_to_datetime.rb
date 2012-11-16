fa5f34fdf4c4357d67b4739a40e5ba697918ce57
Rebecca Nesson <rebecca@prx.org>
2012-11-16 16:02:18 -0500

    getting rid of a migration that postgres doesn't like

--- db/migrate/20121116173102_change_harvest_tentative_date_to_datetime.rb
+++
@@ -1,9 +0,0 @@
-class ChangeHarvestTentativeDateToDatetime < ActiveRecord::Migration
-  def up
-      change_column :harvests, :tentative_date, :datetime
-  end
-
-  def down
-      change_column :harvests, :tentative_date, :string
-  end
-end
