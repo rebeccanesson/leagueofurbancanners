af92c21eeb07c58f32beda1eb5edb38f9a9494ef
Rebecca Nesson <rebecca@prx.org>
2013-01-04 15:06:40 -0500

    sort alphabetically

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,7 +3,7 @@ class PeopleController < ApplicationController
   # GET /people
   # GET /people.json
   def index
-    @people = Person.all
+    @people = Person.order(:last_name, :first_name)
 
     respond_to do |format|
       format.html # index.html.erb
